1d809d2b52e2bebaf62a7e8e45ef2ea15704e3e1
BiancaReset <130721122+BiancaReset@users.noreply.github.com>
2023-07-06 01:33:01 +0000

    Final

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = ["&#9824", "&#9827", "&#9829", "&#9830"];
+  let ranks = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "k"
+  ];
+  let random = Math.random() * ranks.length;
+  let randomSuits = Math.random() * suits.length;
+  let randomCard = Math.floor(random);
+  let randomCardsuits = Math.floor(randomSuits);
+  let currentSuits = suits[randomCardsuits];
+  document.querySelector(".suitsTop").innerHTML = currentSuits;
+  if (currentSuits === "&#9829" || currentSuits === "&#9830") {
+    document.querySelector(".suitsTop").classList.add("red");
+    document.querySelector(".suitsBottom").classList.add("red");
+  }
+  document.querySelector(".suitsBottom").innerHTML = currentSuits;
+  document.querySelector(".ranks").innerHTML = ranks[randomCard];
 };
